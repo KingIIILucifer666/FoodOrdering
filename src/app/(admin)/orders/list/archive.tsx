0e744d175dae28b9f0f4c91573d7c8b5dc26e4d6
sannanhaider <sannanhaider5@gmail.com>
2024-08-07 15:01:08 +0500

    Top Tap Navigator

--- src/app/(admin)/orders/list/archive.tsx
+++ src/app/(admin)/orders/list/archive.tsx
@@ -5,13 +5,10 @@ import { FlatList } from "react-native";
 
 export default function OrdersScreen() {
   return (
-    <>
-      <Stack.Screen options={{ title: "Orders" }} />
-      <FlatList
-        data={orders}
-        contentContainerStyle={{ gap: 10, padding: 10 }}
-        renderItem={({ item }) => <OrderListItem order={item} />}
-      />
-    </>
+    <FlatList
+      data={orders}
+      contentContainerStyle={{ gap: 10, padding: 10 }}
+      renderItem={({ item }) => <OrderListItem order={item} />}
+    />
   );
 }
