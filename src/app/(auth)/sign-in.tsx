cadae10d71625e683c535528c091932da90a51b2
sannanhaider <sannanhaider5@gmail.com>
2024-08-19 13:34:54 +0500

    auth and route protection implemented

--- src/app/(auth)/sign-in.tsx
+++ src/app/(auth)/sign-in.tsx
@@ -1,24 +1,59 @@
-import { View, Text, TextInput, StyleSheet } from "react-native";
+import { View, Text, TextInput, StyleSheet, Alert } from "react-native";
 import React, { useState } from "react";
 import Button from "@/src/components/Button";
 import { Link, Stack } from "expo-router";
 import Colors from "@/src/constants/Colors";
+import { supabase } from "../lib/supabase";
 
 const SignInScreen = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const signInWithEmail = async () => {
+    setLoading(true);
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) {
+      Alert.alert(error.message);
+      setLoading(false);
+      return;
+    }
+    resetFields();
+    setLoading(false);
+  };
+
+  const resetFields = () => {
+    setEmail("");
+    setPassword("");
+  };
 
   return (
     <View style={styles.container}>
       <Stack.Screen options={{ title: "Sign in" }} />
-
       <Text style={styles.label}>Email</Text>
-      <TextInput style={styles.input} />
-
+      <TextInput
+        style={styles.input}
+        value={email}
+        onChangeText={setEmail}
+        placeholder="jon@gmail.com"
+      />
       <Text style={styles.label}>Password</Text>
-      <TextInput style={styles.input} />
-
-      <Button text="Sign in" />
+      <TextInput
+        value={password}
+        onChangeText={setPassword}
+        placeholder="******"
+        style={styles.input}
+        secureTextEntry
+      />
+      <Button
+        text={loading ? "Sign in..." : "Sign in"}
+        onPress={signInWithEmail}
+        disabled={loading}
+      />
       <Link href="/sign-up" style={styles.textButton}>
         Create an account
       </Link>
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -12,6 +12,7 @@ import "react-native-reanimated";
 import CartProvider from "../provider/cartProvider";
 
 import { useColorScheme } from "@/src/components/useColorScheme";
+import AuthProvider from "../provider/AuthProvider";
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -55,14 +56,16 @@ function RootLayoutNav() {
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <CartProvider>
-        <Stack>
-          <Stack.Screen name="(admin)" options={{ headerShown: false }} />
-          <Stack.Screen name="(user)" options={{ headerShown: false }} />
-          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-          <Stack.Screen name="cart" options={{ presentation: "modal" }} />
-        </Stack>
-      </CartProvider>
+      <AuthProvider>
+        <CartProvider>
+          <Stack>
+            <Stack.Screen name="(admin)" options={{ headerShown: false }} />
+            <Stack.Screen name="(user)" options={{ headerShown: false }} />
+            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+            <Stack.Screen name="cart" options={{ presentation: "modal" }} />
+          </Stack>
+        </CartProvider>
+      </AuthProvider>
     </ThemeProvider>
   );
 }
